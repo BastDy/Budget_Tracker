37ec1e14e409a4939ab7fa833c40c4f6941c4cbc
bastien dubray <bastien.dubray@gmail.com>
2024-04-02 15:39:51 +0200

    Add main pages with view

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,14 @@
 class DashboardsController < ApplicationController
+  before_action :set_user, only: [:index]
 
   def index
+    @categories = Categorie.where(user: @current_user)
+    @expenses = Expense.where(user: @current_user)
+  end
 
+  private
+
+  def set_user
+    @user = current_user
   end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,7 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :user
   has_many :categories
 
-  validates :amount, presence: true, numerically: true
+  validates :amount, presence: true, numericality: true
   validates :description, presence: true
   validates :expenses_date, presence: true
 end
